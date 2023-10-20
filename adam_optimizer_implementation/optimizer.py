3c6c529c96d984e9255dfa921d0200b5eabb3597
Hippolyte <Hippolyte.guigon@polytechnique.edu>
2023-10-20 23:51:19 +0200

    created Adam class structure

---
+++ adam_optimizer_implementation/optimizer.py
@@ -0,0 +1,36 @@
+from typing import Dict, Tuple
+
+
+class Adam:
+    """
+    The goal of this class is the implementation
+    of the Adam optimizer algorithm
+
+    Parameters:
+        -params: Dict: The initial parameters
+        to be optimized
+        -learning_rate: float: The learning_rate
+        to be applied
+        -betas: Tuple: The couple of betas to be
+        applied respectively for first and second-order
+        gradient moment
+        -epsilon: float:
+
+    Returns:
+        -None
+    """
+
+    def __init__(
+        self,
+        params: Dict,
+        betas: Tuple(float, float) = (0.9, 0.999),
+        epsilon: float = 1e-08,
+        **kwargs
+    ) -> None:
+        self.params = params
+        self.beta1 = betas[0]
+        self.beta2 = betas[1]
+
+        assert 0 <= self.beta1 <= 1, "beta1 must be between 0 and 1"
+        assert 0 <= self.beta2 <= 1, "beta2 must be between 0 and 1"
+        assert epsilon > 0, "epsilon parameter must be strictly positive"

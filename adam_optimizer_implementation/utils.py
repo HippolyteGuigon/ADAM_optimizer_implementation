5e637f9726aef7afcb4b4af186aa3be0580c3607
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-11-04 16:53:11 +0100

    added stochastic gradient descent

--- adam_optimizer_implementation/utils.py
+++ adam_optimizer_implementation/utils.py
@@ -121,17 +121,18 @@ def launch_training(
     n_total_steps = len(train_loader)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
-    
 
     for epoch in range(num_epochs):
         for i, (images, labels) in enumerate(train_loader):
-            batch_size=images.size()[0]
-            mini_batch_size=int(batch_size/2)
-            
-            permutation=torch.randperm(batch_size)
-            mini_batch_indexes=permutation[:mini_batch_size]
-            image_mini_batch, labels_mini_batch= images[mini_batch_indexes], labels[mini_batch_indexes]
+            batch_size = images.size()[0]
+            mini_batch_size = int(batch_size / 2)
+
+            permutation = torch.randperm(batch_size)
+            mini_batch_indexes = permutation[:mini_batch_size]
+            image_mini_batch, labels_mini_batch = (
+                images[mini_batch_indexes],
+                labels[mini_batch_indexes],
+            )
 
             image_mini_batch = image_mini_batch.reshape(-1, 28 * 28).to(device)
             labels_mini_batch = labels_mini_batch.to(device)
